ffd5062397371d1f646be8a3b7567e0baad9be66
Ubuntu <ubuntu@ip-172-31-39-158.ap-south-1.compute.internal>
2026-06-03 15:43:16 +0000

    Add stay-awake Cask definition

---
+++ Casks/stay-awake.rb
@@ -0,0 +1,16 @@
+cask "stay-awake" do
+  version "1.0.0"
+  sha256 :no_check
+
+  url "https://github.com/princev89/stay-awake/releases/download/v#{version}/Stay.Awake.zip"
+  name "Stay Awake"
+  desc "Keep your Mac awake while builds, scripts, and long-running tasks execute"
+  homepage "https://github.com/princev89/stay-awake"
+
+  app "Stay Awake.app"
+
+  zap trash: [
+    "~/Library/Application Support/StayAwake",
+    "~/Library/LaunchAgents/com.stayawake.app.plist",
+  ]
+end
